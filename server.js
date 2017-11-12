3195776a29eeb5ffa3ddceb1741cb663247db37d
jkmacman <jkellynjus@hotmail.com>
2017-11-11 20:20:54 -0500

    path uodate

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+/* dependencies */
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 
-var jim
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+/* router */
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
+
+/* starts the server */
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
