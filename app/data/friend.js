166aea8cbead68375860aa09fa12c8f8b9605cfc
jkmacman <jkellynjus@hotmail.com>
2017-11-11 20:45:17 -0500

    update

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,3 +1,21 @@
-var jim;
+/* array of friends */
+var friendList = [
+  {
+    name: 'jim kelly',
+    profilePic: 'https://avatars3.githubusercontent.com/u/17382729?s=460&amp;v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
 
-
+module.exports = friendList;
